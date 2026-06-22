8fa862a173b695890ac72663fca9d72b36628684
Davide Cutrupi <davidecutrupi2@gmail.com>
2026-06-22 11:50:29 +0200

    Add MGCoarseGridTrilinosBlockWrapper class to solve directly the coupled coarse layer and refact MGCoarseGridTrilinosWrapper to enable polymorphism

--- include/MultigridWrappers.hpp
+++ include/MultigridWrappers.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
+
+#include <deal.II/base/exception_macros.h>
 #include <deal.II/base/mg_level_object.h>
 #include <deal.II/base/template_constraints.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/types.h>
 
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/lac/la_parallel_block_vector.h>
+#include <deal.II/lac/la_parallel_vector.h>
 #include <deal.II/lac/trilinos_solver.h>
 #include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
 
 #include <deal.II/multigrid/mg_base.h>
 
@@ -44,7 +53,7 @@ namespace solver {
   };
 
 
-  template <typename VectorType>
+  template <unsigned int dim, typename VectorType>
   class MGCoarseGridTrilinosWrapper : public dealii::MGCoarseGridBase<VectorType> {
   public:
     MGCoarseGridTrilinosWrapper() = default;
@@ -55,6 +64,10 @@ namespace solver {
       direct_solver.initialize(coarse_matrix, data);
     }
 
+    void initialize(const dealii::TrilinosWrappers::SparseMatrix &coarse_matrix, const dealii::DoFHandler<dim> &) {
+      initialize(coarse_matrix);
+    }
+
     virtual void operator()(const unsigned int level, VectorType &dst, const VectorType &src) const override {
       (void) level;
       if (temp_src_double.size() == 0) {
@@ -63,7 +76,6 @@ namespace solver {
       }
 
       temp_src_double = src;
-      temp_dst_double = dst;
       direct_solver.vmult(temp_dst_double, temp_src_double);
       dst = temp_dst_double;
     }
@@ -75,4 +87,62 @@ namespace solver {
     mutable dealii::LinearAlgebra::distributed::Vector<double> temp_dst_double;
   };
 
-}
+
+  template <unsigned int dim, typename number>
+  class MGCoarseGridTrilinosBlockWrapper : public dealii::MGCoarseGridBase<dealii::LinearAlgebra::distributed::BlockVector<number>> {
+  public:
+    using BlockVectorType = dealii::LinearAlgebra::distributed::BlockVector<number>;
+
+    void initialize(const dealii::TrilinosWrappers::SparseMatrix &coarse_matrix, const dealii::DoFHandler<dim> &dof_handler) {
+      scalar_size = dof_handler.n_dofs(0);
+      const dealii::IndexSet scalar_owned = dof_handler.locally_owned_mg_dofs(0);
+
+      dealii::IndexSet monolithic_owned(2 * scalar_size);
+      for (const auto index : scalar_owned) {
+        monolithic_owned.add_index(index);
+        monolithic_owned.add_index(index + scalar_size);
+      }
+      monolithic_owned.compress();
+
+      temp_src_double.reinit(monolithic_owned, MPI_COMM_WORLD);
+      temp_dst_double.reinit(monolithic_owned, MPI_COMM_WORLD);
+
+      dealii::TrilinosWrappers::SolverDirect::AdditionalData data;
+      data.solver_type = "Amesos_Mumps";
+      direct_solver.initialize(coarse_matrix, data);
+    }
+
+    void operator()(const unsigned int level, BlockVectorType &dst, const BlockVectorType &src) const override {
+      (void) level;
+      AssertDimension(src.n_blocks(), 2);
+      AssertDimension(dst.n_blocks(), 2);
+
+      const auto n_local = src.block(0).locally_owned_size();
+
+      AssertDimension(src.block(1).locally_owned_size(), n_local);
+      AssertDimension(temp_src_double.locally_owned_size(), 2 * n_local);
+
+      // Fast copy onto monolhitic vector
+      double *start = temp_src_double.begin();
+      for (dealii::types::global_dof_index i = 0; i < n_local; ++i) {
+        start[i] = static_cast<double>(src.block(0).local_element(i));
+        start[i+n_local] = static_cast<double>(src.block(1).local_element(i));
+      }
+
+      // Solve
+      direct_solver.vmult(temp_dst_double, temp_src_double);
+
+      // Fast copy onto the dst block vector
+      start = temp_dst_double.begin();
+      for (dealii::types::global_dof_index i = 0; i < n_local; ++i) {
+        dst.block(0).local_element(i) = static_cast<number>(start[i]);
+        dst.block(1).local_element(i) = static_cast<number>(start[i + n_local]);
+      }
+    }
+
+  private:
+    dealii::types::global_dof_index scalar_size = 0;
+    dealii::TrilinosWrappers::SolverDirect direct_solver;
+    mutable dealii::TrilinosWrappers::MPI::Vector temp_src_double;
+    mutable dealii::TrilinosWrappers::MPI::Vector temp_dst_double;
+  };
