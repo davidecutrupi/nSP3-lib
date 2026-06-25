326da7704ef153746ee5f59f58cb6ad95c7c02d8
Davide Cutrupi <davidecutrupi2@gmail.com>
2026-06-25 18:00:37 +0200

    Add supoort for sigma_f (zero if not present)

--- include/MaterialData.hpp
+++ include/MaterialData.hpp
@@ -3,6 +3,7 @@
 #include <deal.II/base/table.h>
 #include <nlohmann/json.hpp>
 
+#include <cmath>
 #include <fstream>
 #include <stdexcept>
 #include <string>
@@ -20,6 +21,7 @@ namespace data {
       diffusion(n_materials, n_groups),
       sigma_rem(n_materials, n_groups),
       nu_sigma_f(n_materials, n_groups),
+      sigma_f(n_materials, n_groups),
       chi(n_materials, n_groups),
       discontinuity_factor(n_materials, n_groups),
       sigma_s(n_materials, n_groups, n_groups)
@@ -39,6 +41,7 @@ namespace data {
         fill_1d(mat.at("diffusion"), m, diffusion);
         fill_1d(mat.at("sigma_rem"), m, sigma_rem);
         fill_1d(mat.at("nu_sigma_f"), m, nu_sigma_f);
+        fill_1d_or_zero(mat, "sigma_f", m, sigma_f);
         fill_1d(mat.at("chi"), m, chi);
         fill_1d(mat.at("discontinuity_factors"), m, discontinuity_factor);
         fill_2d(mat.at("sigma_s"), m, sigma_s);
@@ -60,6 +63,7 @@ namespace data {
     double get_diffusion(unsigned int m, unsigned int g) const { return diffusion[m][g]; }
     double get_sigma_rem(unsigned int m, unsigned int g) const { return sigma_rem[m][g]; }
     double get_nu_sigma_f(unsigned int m, unsigned int g) const { return nu_sigma_f[m][g]; }
+    double get_sigma_f(unsigned int m, unsigned int g) const { return sigma_f[m][g]; }
     double get_chi(unsigned int m, unsigned int g) const { return chi[m][g]; }
     double get_discontinuity_factor(unsigned int m, unsigned int g) const { return discontinuity_factor[m][g]; }
     double get_sigma_s(unsigned int m, unsigned int g_from, unsigned int g_to) const { return sigma_s[m][g_from][g_to]; }
@@ -81,6 +85,7 @@ namespace data {
     Table<2, double> diffusion;
     Table<2, double> sigma_rem;
     Table<2, double> nu_sigma_f;
+    Table<2, double> sigma_f;
     Table<2, double> chi;
     Table<2, double> discontinuity_factor;
     Table<3, double> sigma_s;
@@ -93,6 +98,17 @@ namespace data {
 
       for (unsigned int g = 0; g < n_groups; ++g)
         table[m][g] = arr[g].get<double>();
+    }
+
+
+    void fill_1d_or_zero(const json &mat, const std::string &field_name, unsigned int m, Table<2, double> &table) const {
+      if (mat.contains(field_name)) {
+        fill_1d(mat.at(field_name), m, table);
+        return;
+      }
+
+      for (unsigned int g = 0; g < n_groups; ++g)
+        table[m][g] = 0.0;
     }
 
 
